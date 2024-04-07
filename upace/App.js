ec273b9f89150fa8ef1949cadc0dc28c493db261
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-04-07 10:04:19 +0300

    feat: theme applied to App.js

--- upace/App.js
+++ upace/App.js
@@ -1,15 +1,27 @@
-import { PaperProvider, MD3LightTheme, adaptNavigationTheme } from "react-native-paper";
+import { PaperProvider, MD3LightTheme, MD3DarkTheme, adaptNavigationTheme } from "react-native-paper";
 import TimeCalc from "./screens/timeCalc";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme, DarkTheme as NavigatorDarkTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import light from "./themes/light";
+import dark from "./themes/dark";
+import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
-const { LightTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme });
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+    reactNavigationLight: DefaultTheme,
+    reactNavigationDark: NavigatorDarkTheme,
+});
 
 export default function App() {
+    const theme = {
+        ...MD3DarkTheme,
+        ...dark,
+    };
+
     return (
-        <PaperProvider theme={MD3LightTheme}>
-            <NavigationContainer theme={LightTheme}>
+        <PaperProvider theme={theme}>
+            <StatusBar style={"light"} />
+            <NavigationContainer theme={DarkTheme}>
                 <Stack.Navigator>
                     <Stack.Screen name="Home" component={TimeCalc} />
                 </Stack.Navigator>
